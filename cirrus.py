455a5811ee35d225d4ebbccdc1586a63939d1c61
Clayton O'Neill <clayton.oneill@twcable.com>
2015-04-28 19:41:18 +0000

    PEP8 fixes + testing
    Add in initial tox and setup tools config.
    
    Change-Id: I292853b526ca1cdcf258f57a81bbc5b2df6b5917

--- cirrus.py
+++ cirrus.py
@@ -1,10 +1,9 @@
 import re
-import IPython
 
 from designate.notification_handler.base import BaseAddressHandler
 from designate.openstack.common import log as logging
-import designateclient.v1 as designate_c
 import designateclient.exceptions
+import designateclient.v1 as designate_c
 from keystoneclient.v2_0 import client as keystone_c
 from neutronclient.v2_0 import client as neutron_c
 from novaclient.v2 import client as nova_c
@@ -38,16 +37,17 @@ def get_instance_info(auth_url, tenant_id, token, port_id):
     nc = neutron_c.Client(token=token, endpoint_url=neutron_endpoint)
     port_details = nc.show_port(port_id)
     instance_id = port_details['port']['device_id']
-    instance_info = { 'id': instance_id }
+    instance_info = {'id': instance_id}
 
     nova_endpoint = kc.service_catalog.url_for(service_type='compute',
                                                endpoint_type='internalURL')
     nvc = nova_c.Client(auth_token=token, bypass_url=nova_endpoint)
-    server_info = nvc.servers.get(instance_id) 
+    server_info = nvc.servers.get(instance_id)
     instance_info['name'] = server_info.name
     instance_info['tenant_id'] = server_info.tenant_id
 
     return instance_info
+
 
 def pick_tenant_domain(client, regex, metadata={}):
     tenant_domains = client.domains.list()
@@ -60,6 +60,7 @@ def pick_tenant_domain(client, regex, metadata={}):
         if 'description' in domain and domain['description'] is not None:
             if re.search(regex, domain['description']):
                 return domain
+
 
 def create_record(client, domain_id, name, fip):
     record = {
@@ -74,18 +75,20 @@ def create_record(client, domain_id, name, fip):
     client.records.create(domain_id, record)
     LOG.info('Creating %s record for FIP %s' % (name, fip))
 
+
 def associate_floating_ip(client, domain_id, name, fip):
-    try: 
+    try:
         create_record(client, domain_id, name, fip)
     except designateclient.exceptions.Conflict:
         # If there is already a client with this name, then we append the ip
         # address to the end, with the dots converted to dashes
-        octets = [ int(x) for x in fip.split('.') ]
+        octets = [int(x) for x in fip.split('.')]
         fqdnparts = name.split('.')
         fqdnparts[0] = "%s-%d-%d-%d-%d" % tuple([fqdnparts[0]] + octets)
         new_name = '.'.join(fqdnparts)
         LOG.warn('Could not create %s, trying %s instead' % (name, new_name))
         create_record(client, domain_id, new_name, fip)
+
 
 def purge_fip_in_domain(client, domain, fip):
     LOG.debug('Looking for A records matching %s in %s(%s)' % (fip, domain['name'], domain['id']))
@@ -104,6 +107,7 @@ def disassociate_floating_ip(client, fip):
     for domain in tenant_domains:
         delete_count += purge_fip_in_domain(client, domain, fip)
     LOG.info('Deleted %d records that matched %s' % (delete_count, fip))
+
 
 class CirrusFloatingHandler(BaseAddressHandler):
     """Handler for Neutron's notifications."""
@@ -165,8 +169,8 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description="Test Cirrus notification handler")
     subparsers = parser.add_subparsers(title='subcommands',
-            description='valid subcommands',
-            help='additional help')
+                                       description='valid subcommands',
+                                       help='additional help')
 
     parser.add_argument("--endpoint-type",
                         metavar='<endpoint-type>',
@@ -209,35 +213,37 @@ def parse_args():
                              'Defaults to env[OS_AUTH_URL].')
 
     gii_parser = subparsers.add_parser('get-instance-info',
-            help='Get instance info from Neutron port-id')
+                                       help='Get instance info from Neutron port-id')
     gii_parser.add_argument('--port-id', required=True,
-            help='Neutron port-id to get info for')
+                            help='Neutron port-id to get info for')
     gii_parser.set_defaults(func=test_get_instance_info)
 
     ptd_parser = subparsers.add_parser('pick-tenant-domain',
-            help='Given a tenant-id, return the domain name to put FIP records in')
+                                       help='Given a tenant-id, return the '
+                                            'domain name to put FIP records in')
     ptd_parser.add_argument('--tenant-id', required=True,
-            help='ID of tenant to look up in Designate')
+                            help='ID of tenant to look up in Designate')
     ptd_parser.add_argument('--domain-desc-regex', default='\(default\)$',
-            metavar='REGEX',
-            help='Regex to match against designate domain description when '
-                 'picking among multiples.  Defaults to \'\(default\)$\'')
+                            metavar='REGEX',
+                            help='Regex to match against designate domain description when '
+                                 'picking among multiples.  Defaults to \'\(default\)$\'')
     ptd_parser.set_defaults(func=test_pick_tenant_domain)
 
     afi_parser = subparsers.add_parser('associate-floating-ip',
-            help='Given a IP address and domain ID, create a new record in the default domain')
+                                       help='Given a IP address and domain ID, create a new record in the default domain')
     afi_parser.add_argument('--domain-id', required=True,
-            help='UUID of designate-domain to create record in')
+                            help='UUID of designate-domain to create record in')
     afi_parser.add_argument('--name', required=True,
-            help='Name of new record to create')
+                            help='Name of new record to create')
     afi_parser.add_argument('--ip-address', required=True,
-            help='IP Address to associate with record')
+                            help='IP Address to associate with record')
     afi_parser.set_defaults(func=test_associate_floating_ip)
 
     dfi_parser = subparsers.add_parser('disassociate-floating-ip',
-            help='Given a IP address, remove any A records in any tenant domains that match')
+                                       help='Given a IP address, remove any A '
+                                            'records in any tenant domains that match')
     dfi_parser.add_argument('--ip-address', required=True,
-            help='IP Address to disassociate with records found')
+                            help='IP Address to disassociate with records found')
     dfi_parser.set_defaults(func=test_disassociate_floating_ip)
 
     return parser.parse_args()
@@ -250,7 +256,7 @@ def test_get_instance_info(kc, args):
 def test_pick_tenant_domain(kc, args):
     designate_endpoint = kc.service_catalog.url_for(service_type='dns',
                                                     endpoint_type='internalURL')
-    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id, 
+    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id,
                             endpoint=designate_endpoint)
     print(pick_tenant_domain(client=dc, regex=args.domain_desc_regex))
 
@@ -258,16 +264,18 @@ def test_pick_tenant_domain(kc, args):
 def test_associate_floating_ip(kc, args):
     designate_endpoint = kc.service_catalog.url_for(service_type='dns',
                                                     endpoint_type='internalURL')
-    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id, 
+    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id,
                             endpoint=designate_endpoint)
     associate_floating_ip(client=dc, domain_id=args.domain_id, name=args.name, fip=args.ip_address)
- 
+
+
 def test_disassociate_floating_ip(kc, args):
     designate_endpoint = kc.service_catalog.url_for(service_type='dns',
                                                     endpoint_type='internalURL')
-    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id, 
+    dc = designate_c.Client(token=kc.auth_token, tenant_id=kc.auth_tenant_id,
                             endpoint=designate_endpoint)
     disassociate_floating_ip(client=dc, fip=args.ip_address)
+
 
 def main():
     args = parse_args()
