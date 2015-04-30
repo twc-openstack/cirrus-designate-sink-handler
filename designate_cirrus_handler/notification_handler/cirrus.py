de98e0c396b6bff1551d37b9207db053e3645eb4
Clayton O'Neill <clayton.oneill@twcable.com>
2015-04-30 00:32:19 +0000

    Rename keystone_authurl param
    Renamed to keystone_auth_uri to better match other services.
    
    Change-Id: I8fa2de72c49987d2d522d19c06fb19eca9877279

--- designate_cirrus_handler/notification_handler/cirrus.py
+++ designate_cirrus_handler/notification_handler/cirrus.py
@@ -22,7 +22,7 @@ cfg.CONF.register_opts([
     cfg.StrOpt('control-exchange', default='neutron'),
     cfg.StrOpt('domain-id', default=None),
     cfg.StrOpt('format', default=None),
-    cfg.StrOpt('keystone_authurl', default=None),
+    cfg.StrOpt('keystone_auth_uri', default=None),
     cfg.StrOpt('region', default=None),
     cfg.StrOpt('default_regex', default='\(default\)$'),
 ], group='handler:cirrus_floatingip')
