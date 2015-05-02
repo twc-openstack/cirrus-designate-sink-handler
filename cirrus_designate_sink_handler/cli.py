4c34d816291ff5106444dfe9c8b2d5f8334c2280
Clayton O'Neill <clayton.oneill@twcable.com>
2015-05-02 19:07:03 +0000

    Added license information
    Restored license information found in original neutron example
    notification handler, added TWC copyright and Apache License
    information.
    
    Change-Id: I16b248d4c4a1980b3aa889f4ae982dfd97d9b7b8

--- cirrus_designate_sink_handler/cli.py
+++ cirrus_designate_sink_handler/cli.py
@@ -1,3 +1,19 @@
+# Copyright 2015 Time Warner Cable
+#
+# Author: Clayton O'Neill <clayton.oneill@twcable.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import cirrus_designate_sink_handler.notification_handler.cirrus_floating_ip_handler as cirrus
 from designate.openstack.common import log as logging
 from designate.utils import find_config
